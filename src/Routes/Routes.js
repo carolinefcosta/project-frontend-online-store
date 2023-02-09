041a6ad33ae4436e7d82f960e7273b6c90c7d4ba
Caroline <carolinefernandesc@hotmail.com>
2023-02-09 19:33:04 -0300

    requisito 03 finalizado, projeto realizado em grupo, Caroline, Josiane, Gregório, Patrick

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
+import ShoppingCart from '../pages/ShoppingCart';
 
 class Routes extends Component {
   render() {
     return (
       <Switch>
-        <Route path="/" component={ Home } />
+        <Route path="/shopping" component={ ShoppingCart } />
+        <Route exact path="/" component={ Home } />
       </Switch>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
-      <label>
-        <input
-          type="text"
-        />
-      </label>
+      <>
+        <label>
+          <input
+            type="text"
+          />
+        </label>
+        {/* <button
+          type="button"
+        >
+          Meu Carrinho
+
+        </button> */}
+        <Link to="/shopping" data-testid="shopping-cart-button">Meu Carrinho</Link>
+      </>
     );
   }
 }
