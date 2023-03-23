4eda9126969708ac66b4095d04847df13cb4bce0
Caroline <carolinefernandesc@hotmail.com>
2023-03-23 15:11:32 -0300

    Estilizando

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import Category from '../components/Category';
 import Header from '../components/Header';
 import ProductCard from '../components/ProductCard';
 import '../styles/Home.css';
+import carrinhoVazio from '../img/carrinho-vazio.png';
 
 class Home extends Component {
   render() {
@@ -17,51 +18,58 @@ class Home extends Component {
       addToCart,
     } = this.props;
     return (
-      <div>
+      <>
         <Header
           value={ inputSearch }
           onChange={ handleChange }
           onClick={ () => getProductsFromApi(inputSearch) }
         />
-        <Category
-          listCategory={ listCategory }
-          onClick={ getProductsFromCategory }
-        />
-        <div className="home-div">
-          {(resultProducts.length < 1) && (
-            <h1 className="home-h1">Nenhum produto foi encontrado</h1>
-          )}
-          {
-            (!resultProducts.length < 1) ? (
-              resultProducts.map((product) => (
-                <div key={ product.id } className="home-product">
-                  <ProductCard
-                    dataTestId="product"
-                    dataTestButton="product-add-to-cart"
-                    addToCart={ addToCart }
-                    product={ product }
-                  />
-                  <Link
-                    className="home-details"
-                    data-testid="product-detail-link"
-                    to={ `/productDetails/${product.id}` }
-                  >
-                    Detalhes
-                  </Link>
-                </div>
-              ))
+        <div className="main-div">
 
-            ) : (
-              <h1
-                className="home-h1-2"
-                data-testid="home-initial-message"
-              >
-                Digite algum termo de pesquisa ou escolha uma categoria.
-              </h1>
-            )
-          }
+          <Category
+            listCategory={ listCategory }
+            onClick={ getProductsFromCategory }
+          />
+          <div className="home-div">
+            {(resultProducts.length < 1) && (
+              <div className="div-h1-img">
+                <h1 className="home-h1">Nenhum produto foi encontrado</h1>
+                <img src={ carrinhoVazio } alt="carrinho-vazio" />
+              </div>
+
+            )}
+            {
+              (!resultProducts.length < 1) ? (
+                resultProducts.map((product) => (
+                  <div key={ product.id } className="home-product">
+                    <ProductCard
+                      dataTestId="product"
+                      dataTestButton="product-add-to-cart"
+                      addToCart={ addToCart }
+                      product={ product }
+                    />
+                    <Link
+                      className="home-details"
+                      data-testid="product-detail-link"
+                      to={ `/productDetails/${product.id}` }
+                    >
+                      Detalhes
+                    </Link>
+                  </div>
+                ))
+
+              ) : (
+                <h1
+                  className="home-h1-2"
+                  data-testid="home-initial-message"
+                >
+                  Digite algum termo de pesquisa ou escolha uma categoria.
+                </h1>
+              )
+            }
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 }
